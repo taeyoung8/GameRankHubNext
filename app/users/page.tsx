44f3541c70bc0adfe53721c3b2726764e1478d71
taeyoungkim97 <taeyoung.kim.3@stonybrook.edu>
2024-07-12 17:12:59 -0400

    basic

---
+++ app/users/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch(
+    'https://jsonplaceholder.typicode.com/users',
+    {cache: 'no-store'});
+    // { next: { revalidate: 10}});
+  const users: User[] = await res.json();
+
+  return (
+    <>
+    <h1>Users</h1>
+    <p>{new Date().toLocaleTimeString()}</p> {/* when the time is rendered - static page */}
+    <ul>
+      {users.map(user => <li key={user.id}>{user.name}</li>)}
+    </ul>
+    </>
+  )
+}
+
+export default UsersPage
