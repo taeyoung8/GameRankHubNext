ada67df6d553f5e9fcc060edaa1f7240c2e89886
taeyoungkim97 <taeyoung.kim.3@stonybrook.edu>
2024-07-15 11:53:56 -0400

    added Header

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-// pages/index.tsx
 import Link from 'next/link';
 import ProductCard from './components/ProductCard';
 import Header from './components/Header';
@@ -7,7 +6,7 @@ const Home = () => {
   return (
     <main>
       {/* Client-side navigation */}
-      <Header />
+      {/* <Header /> */}
       <Link href="/users">Users</Link>
       <ProductCard />
     </main>
